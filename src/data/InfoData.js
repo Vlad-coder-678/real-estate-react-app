07a10200cde6957704def2e25a5689a7b0a9ffe2
Vlad-coder-678 <vova.mezencev@rambler.ru>
2021-07-09 09:16:05 +0300

    first adding

---
+++ src/data/InfoData.js
@@ -0,0 +1,26 @@
+import Image1 from "../images/interior-1.jpg";
+import Image2 from "../images/house-5.jpg";
+
+export const InfoData = {
+  heading: "Explore our beuytifull homes",
+  paragraphOne:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores expedita modi dolorum saepe culpa minima necessitatibus voluptatem dolor non. Reiciendis.",
+  paragraphTwo:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores expedita modi dolorum saepe culpa.",
+  buttonLabel: "View Homes",
+  image: Image1,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: "Modern Designs",
+  paragraphOne:
+    "Lorem ipsum dolor consectetur adipisicing elit. Dolores expedita modi dolorum saepe culpa minima necessitatibus voluptatem dolor non. Reiciendis.",
+  paragraphTwo:
+    "Lorem ipsum dolor sit. Dolores expedita modi dolorum saepe culpa.",
+  buttonLabel: "View Homes",
+  image: Image2,
+  reverse: true,
+  delay: 300,
+};
